6489ab20148c04296607218d78ce6cae8fa79f6e
Marimar <malonso@quadram.mobi>
2023-05-15 20:20:34 +0200

    add lista categorias en angular

--- src/app/entities/category/service/category.service.ts
+++ src/app/entities/category/service/category.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../model/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient   ) { }
+
+  public getAllCategories(): Observable<Category[]>{
+
+    const urlEndpoint: string = "http://localhost:8080/store/categories";
+    return this.httpClient.get<Category[]>(urlEndpoint);
+
+  }
 }
