d9c1cb58e3ac01be0488c524c0e74f626550e4f9
Marimar <malonso@quadram.mobi>
2023-05-16 10:39:05 +0200

    mostrar categorias, items, items por categoria, interceptor de errores

--- src/app/entities/item/item-list/item-list.component.ts
+++ src/app/entities/item/item-list/item-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../service/item.service';
+import { Item } from '../modelo/item.model';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss']
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnInit{
+
+  items: Item [] = [];
+  categoryId?: number;
+  title: string ="";
+
+  constructor(private route: ActivatedRoute,
+              private itemService: ItemService){}
+
+  ngOnInit(): void {
+    if(this.route.snapshot.paramMap.get("categoryId")){
+      //Para cambiar un number a cadera poniendo un + delante y ! por que puede ser nulo
+      this.categoryId=+this.route.snapshot.paramMap.get("categoryId")!;
+      this.title = "Artículos de la categoría "+ this.categoryId;
+      this.getAllItemsInCategoryId(this.categoryId);
+    }else{
+      this.title = "Lista de artículos";
+      this.getAllItems();
+    }
+  }
+  private getAllItems(): void{
+    this.itemService.getAllItems().subscribe ({
+      next: (itemsRequest) => { this.items = itemsRequest; },
+      error: (err) => { this.handleError(err); }
+    })
+  }
+  private getAllItemsInCategoryId(categoryId: number): void{
+    this.itemService.getAllItemsByCategoryId(categoryId).subscribe ({
+      next: (itemsRequest) => { this.items = itemsRequest; },
+      error: (err) => { this.handleError(err); }
+    })
+  }
+
+  private handleError(error: any): void{
+    //Lo que queramos que haga por ejemplo mostrar un alert al usuario
+  }
+
+
 
 }
--- src/app/entities/item/service/item.service.ts
+++ src/app/entities/item/service/item.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item } from '../modelo/item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient   ) { }
+
+  public getAllItems(): Observable<Item[]>{
+    const urlEndpoint: string = "http://localhost:8080/store/items";
+    return this.httpClient.get<Item[]>(urlEndpoint);
+  }
+  public getAllItemsByCategoryId(categoryId: number): Observable<Item[]>{
+    const urlEndpoint: string = "http://localhost:8080/store/categories/" + categoryId + "/items";
+    return this.httpClient.get<Item[]>(urlEndpoint);
+  }
+
+
 }
