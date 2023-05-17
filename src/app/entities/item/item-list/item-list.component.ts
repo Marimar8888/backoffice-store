4441577bd93dd6f26fdfef7077930225c1475ea3
Marimar <malonso@quadram.mobi>
2023-05-17 11:11:22 +0200

    paginación

--- src/app/entities/item/item-list/item-list.component.ts
+++ src/app/entities/item/item-list/item-list.component.ts
@@ -14,6 +14,16 @@ export class ItemListComponent implements OnInit{
   categoryId?: number;
   title: string ="";
 
+  page: number =0;
+  size: number = 1;
+  sort: string = "name,asc";
+
+  first: boolean = false;
+  last: boolean = false;
+  totalPages: number = 0;
+  totalElements: number = 0;
+
+
   constructor(private route: ActivatedRoute,
               private itemService: ItemService){}
 
@@ -29,10 +39,17 @@ export class ItemListComponent implements OnInit{
     }
   }
   private getAllItems(): void{
-    this.itemService.getAllItems().subscribe ({
-      next: (itemsRequest) => { this.items = itemsRequest; },
+    this.itemService.getAllItems(this.page, this.size, this.sort).subscribe ({
+      next: (data:any) => {
+        this.items = data.content;
+        this.first = data.first;
+        this.last = data.last;
+        this.totalPages = data.totalPages;
+        this.totalElements = data.totalElements;
+      },
       error: (err) => { this.handleError(err); }
     })
+    console.log(this.items);
   }
   private getAllItemsInCategoryId(categoryId: number): void{
     this.itemService.getAllItemsByCategoryId(categoryId).subscribe ({
@@ -43,6 +60,15 @@ export class ItemListComponent implements OnInit{
 
   private handleError(error: any): void{
     //Lo que queramos que haga por ejemplo mostrar un alert al usuario
+  }
+
+  public nextPage():void{
+    this.page++;
+    this.getAllItems();
+  }
+  public previousPage():void{
+    this.page--;
+    this.getAllItems();
   }
 
 
--- src/app/entities/item/service/item.service.ts
+++ src/app/entities/item/service/item.service.ts
@@ -10,8 +10,8 @@ export class ItemService {
 
   constructor(private httpClient: HttpClient   ) { }
 
-  public getAllItems(): Observable<Item[]>{
-    const urlEndpoint: string = "http://localhost:8080/store/items";
+  public getAllItems(page: number, size: number, sort: string): Observable<Item[]>{
+    const urlEndpoint: string = "http://localhost:8080/store/items?page=" + page + "&size=" + size + "&sort=" + sort;
     return this.httpClient.get<Item[]>(urlEndpoint);
   }
   public getAllItemsByCategoryId(categoryId: number): Observable<Item[]>{
